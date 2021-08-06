6d044f5f4caf7eb23faa160eed5d3347ca34e4aa
AlmusFuad <almusfuad@gmail.com>
2021-08-06 17:49:00 +0600

    app drawer to switch

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../screens/orders_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('Menu'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('orders'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
