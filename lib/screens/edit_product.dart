c21623f82be49489021f41e80c0fc0af336a6aea
AlmusFuad <almusfuad@gmail.com>
2021-08-11 23:54:00 +0600

    input validation

--- lib/screens/edit_product.dart
+++ lib/screens/edit_product.dart
@@ -46,6 +46,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
     _form.currentState.save();
   }
 
@@ -72,6 +76,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please entered a title.';
+                  }
+                  return value = null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
