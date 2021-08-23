9eaf811e9f791f38062ab9b5d6a4031a3d59662d
AlmusFuad <almusfuad@gmail.com>
2021-08-23 11:16:33 +0600

    refresh indicator

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -121,9 +121,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url =
+          'https://flutter-newapp-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json';
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.description,
+            'price': newProduct.price,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
