644e6fb70a7963bdc9220bad80bc8ce646936c77
AlmusFuad <almusfuad@gmail.com>
2021-09-10 02:36:51 +0600

    managing orders on server

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -22,8 +22,9 @@ class OrderItem {
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
   final String authToken;
+  final String userId;
 
-  Orders(this.authToken, this._orders);
+  Orders(this.authToken, this.userId, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
@@ -31,7 +32,7 @@ class Orders with ChangeNotifier {
 
   Future<void> fetchAndSetOrders() async {
     final url =
-        'https://flutter-newapp-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json?auth=$authToken';
+        'https://flutter-newapp-default-rtdb.asia-southeast1.firebasedatabase.app/orders/$userId.json?auth=$authToken';
 
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
@@ -63,8 +64,8 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    const url =
-        'https://flutter-newapp-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json';
+    final url =
+        'https://flutter-newapp-default-rtdb.asia-southeast1.firebasedatabase.app/orders/$userId.json?auth=$authToken';
     final timeStamp = DateTime.now();
     final response = await http.post(
       url,
