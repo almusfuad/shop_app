906e7f3b26860e857fe8cd2031ff3e92c934e2b8
AlmusFuad <almusfuad@gmail.com>
2021-09-09 11:53:13 +0600

    using proxy providers

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,10 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(
           value: Auth(),
         ),
-        ChangeNotifierProvider.value(
-          value: Products(),
+        ChangeNotifierProxyProvider<Auth, Products>(
+          create: null,
+          update: (ctx, auth, previousProducts) => Products(auth.token,
+              previousProducts == null ? [] : previousProducts.items),
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
