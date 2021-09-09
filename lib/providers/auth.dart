b1432a0ac3fd2a21725dca35ef8356adaa777472
AlmusFuad <almusfuad@gmail.com>
2021-09-10 02:45:22 +0600

    manually logout

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -68,4 +68,11 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logOut() {
+    _token = null;
+    _expiryDate = null;
+    _userId = null;
+    notifyListeners();
+  }
 }
